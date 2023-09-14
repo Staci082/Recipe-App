0e69509035266d2020d962265de117b659f35560
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-09-14 11:51:08 +0200

    cleaning

--- src/Components/recipes-container/RecipesContainer.jsx
+++ src/Components/recipes-container/RecipesContainer.jsx
@@ -1,83 +1,21 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import Categories from "../categories/Categories";
-import UseGetUserId from "../../Hooks/useGetUserId";
-import { FaHeart, FaRegHeart } from "react-icons/fa6";
-import { useParams, Routes, Route, Outlet } from "react-router-dom";
+import Pagination from "../../Components/pagination/Pagination";
+import { Outlet } from "react-router-dom";
 import DiscoverRecipes from "../discover-recipes/DiscoverRecipes";
 import Dessert from "../dessert/Dessert";
 
 function RecipesContainer() {
-    // const { route } = useParams();
-
-    // // FILTER RECIPES
-    // const [recipes, setRecipes] = useState([]);
-
-    // // SAVE RECIPES
-    // const [savedRecipes, setSavedRecipes] = useState([]);
-    // const [saveRecipeButton, setSaveRecipeButton] = useState([]);
-
-    // const userID = UseGetUserId();
-
-    // useEffect(() => {
-    //     const fetchRecipes = async () => {
-    //         try {
-    //             const response = await axios.get("http://localhost:5712/");
-    //             setRecipes(response.data);
-    //         } catch (error) {
-    //             console.log("Error fetching data:", error);
-    //         }
-    //     };
-    //     // const fetchSavedRecipes = async () => {
-    //     //     try {
-    //     //         const response = await axios.get(`http://localhost:5712/savedrecipes/ids/${userID}`);
-    //     //         setSavedRecipes(response.data);
-    //     //     } catch (error) {
-    //     //         console.log("Error fetching data:", error);
-    //     //     }
-    //     // };
-
-    //     // const fetchLikes = async () => {
-    //     //     try {
-    //     //         const response = await axios.get(`http://localhost:5712/auth/getlikes/${userID}`, {
-    //     //             headers: { Authorization: `Bearer ${document.cookie.split("=")[1]}` },
-    //     //         });
-
-    //     //         setSaveRecipeButton(response.data.likes);
-    //     //     } catch (error) {
-    //     //         console.log("Error fetching data:", error);
-    //     //     }
-    //     // };
-
-    //     // fetchSavedRecipes();
-    //     // fetchLikes();
-    //     fetchRecipes();
-    // }, [route]);
-
-    // const saveRecipe = async (recipeID) => {
-    //     try {
-    //         const response = await axios.put("http://localhost:5712/", { recipeID, userID });
-    //         console.log(response);
-    //         // setSaveRecipeButton(!saveRecipeButton);
-    //         setSaveRecipeButton([...saveRecipeButton,recipeID])
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
-  
     return (
-        <div className="recipe-container">
-            <Categories />
+        <>
+            <div className="recipe-container">
+                <Categories />
 
+                {/* Outlet provides this space with the ever changing component data */}
+                <Outlet />
+            </div>
 
-            <Outlet/>
-
-            <a className="recipe" href="/recipe"></a>
-            <a className="recipe" href="/recipe"></a>
-            <a className="recipe" href="/recipe"></a>
-            <a className="recipe" href="/recipe"></a>
-            <a className="recipe" href="/recipe"></a>
-        </div>
+            <Pagination />
+        </>
     );
 }
 
--- src/Pages/homepage/Homepage.jsx
+++ src/Pages/homepage/Homepage.jsx
@@ -2,7 +2,6 @@
 
 // IMPORT COMPONENTS
 import Header from "../../Components/header/Header";
-import Pagination from "../../Components/pagination/Pagination";
 import RecipesContainer from "../../Components/recipes-container/RecipesContainer";
 import Menu from "../../Components/menu/Menu";
 
@@ -19,7 +18,6 @@ function Homepage() {
         <div className="main-container">
             <Menu />
             <RecipesContainer />
-            <Pagination />
         </div>
 
 
