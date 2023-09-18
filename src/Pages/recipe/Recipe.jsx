b2657bc4b1fa3149cc35b89232a3402d469a4826
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-09-18 14:24:29 +0200

    single recipe data fetch working

--- src/Pages/recipe/Recipe.jsx
+++ src/Pages/recipe/Recipe.jsx
@@ -13,7 +13,6 @@ function Recipe() {
         const fetchRecipe = async () => {
             try {
                 const response = await axios.get("http://localhost:5712/recipe/" + recipeID);
-                console.log(response.data);
                 setRecipe(response.data);
             } catch (error) {
                 console.log("Error fetching data:", error);
@@ -32,17 +31,18 @@ function Recipe() {
                     </a>
                     <div className="recipe-details-container">
                         <div className="ingredients-container">
-                            {console.log(recipe)}
                             <ul>
-                                {/* {recipe.ingredients.map((item) => (
+                                {/* have to get values after data is fetched */}
+                                {recipe.ingredients && recipe.ingredients.map((item) => (
                                     <li key={item}>{item}</li>
-                                ))} */}
+                                ))}
                             </ul>
                         </div>
                         <div className="instructions-container">
-                            {/* {recipe.instructions.map((item) => (
-                                <li key={item}>{item}</li>
-                            ))} */}
+                        {/* {recipe.instructions && recipe.instructions.map((item) => (
+                                    <li key={item}>{item}</li>
+                                ))} */}
+                            {recipe.instructions}
                         </div>
                         <div className="recipe-button-container">
                             <a href="/edit/{_id}" className="edit-buttons">
