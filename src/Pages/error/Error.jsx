2b87dea7d60b675c4fccf683dec17d9a9c0e86c2
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-10-10 08:54:36 +0200

    search bug fixed

--- src/Pages/error/Error.jsx
+++ src/Pages/error/Error.jsx
@@ -1,17 +1,18 @@
-import taco from "/runningtaco.gif"
+import taco from "/runningtaco.gif";
 
 function Error() {
-  return (
-    <>
-     <div className="no-auth-container">
-    <a href="/" className="form-back-button">
-        &times;
-    </a>
-        <p className="no-auth-text">Page not found.</p> 
-    <img src={taco} alt="taco gif" className="taco-gif"/>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="no-auth-container">
+                <a href="/" className="form-back-button">
+                    &times;
+                </a>
+                <p className="no-auth-text">404</p>
+                <p className="no-auth-text">Page not found.</p>
+                <img src={taco} alt="taco gif" className="taco-gif" />
+            </div>
+        </>
+    );
 }
 
-export default Error
+export default Error;
